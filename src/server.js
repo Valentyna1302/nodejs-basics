805ebc7416166d3c63c7ff2b4a43ef7809b5d9e6
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-03-31 13:00:34 +0200

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import cors from 'cors';
 import express from 'express';
 import pino from 'pino-http';
-import { getAllStudents, getStudentById } from './services/students.js';
+
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import studentsRouter from './routes/students.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -26,43 +29,9 @@ export const startServer = () => {
     });
   });
 
-  app.get('/students', async (req, res) => {
-    const students = await getAllStudents();
-
-    res.status(200).json({
-      data: students,
-    });
-  });
-
-  app.get('/students/:studentId', async (req, res, next) => {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    if (!student) {
-      res.status(404).json({
-        message: 'Student not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      data: student,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(studentsRouter);
+  app.use('*', notFoundHandler);
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is runnung on port ${PORT}`);
